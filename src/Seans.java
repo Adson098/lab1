6b877dfb640a3ad4dcd1996101631f8a145efe7d
unknown <Adson098>
2022-11-05 16:03:27 +0100

    seans.java:

--- src/Seans.java
+++ src/Seans.java
@@ -1,5 +1,18 @@
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Seans {
-    HashMap<Character, HashMap<Character,Integer>> gowno;
+    // Zmienne
+    private String title;
+    private Date day;
+    private LocalTime time;
+    private int ageRestrictions;
+
+    private  HashMap<Character, HashMap<Integer, Boolean>> liczbaMiejsc;
+
+    public Seans(){
+    }
+
+
 }
