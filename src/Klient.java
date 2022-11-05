6d758bb12b5f728c07896a0f7498841353af7bb2
unknown <Adson098>
2022-11-05 15:42:36 +0100

    klasa klient

---
+++ src/Klient.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Klient {
+    private String nazwisko;
+    private String email;
+    private String telefon;
+    private String seans;
+    private List<String> miejsca;
+
+    public Klient(){
+        System.out.print("gowno");
+    }
+}
