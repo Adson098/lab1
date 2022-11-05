9405843ced78598044bd79b3ba0eb9425f6ca1b3
unknown <Adson098>
2022-11-05 16:00:19 +0100

    read/write file

--- src/main.java
+++ src/main.java
@@ -1,4 +1,25 @@
+import java.io.*;
+
 public class main {
+
+
+    public void saveFile(String data) throws IOException {
+        FileWriter f = new FileWriter("temp.txt");
+        BufferedWriter out = new BufferedWriter(f);
+        out.write(data);
+        out.close();
+    }
+
+    public void readFile() throws IOException {
+        FileInputStream f = new FileInputStream("temp.txt");
+        DataInputStream in = new DataInputStream(f);
+        BufferedReader r = new BufferedReader(new InputStreamReader(in));
+        String strLine;
+        String dane="";
+        while ((strLine = r.readLine()) != null)
+            dane += strLine;
+        in.close();
+    }
 
     public static void main(String []args){
         System.out.println("gowno");
